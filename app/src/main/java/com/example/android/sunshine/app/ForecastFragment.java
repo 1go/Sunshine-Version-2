d9bb7a925f05d7b594d8b71df9e5223469866a28
1go <ariposati@gmail.com>
2015-07-09 17:20:10 -0400

    Update ForecastFragment.java
    Typo: the underscore was missing.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -64,7 +64,7 @@ public class ForecastFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.forecastfragment, menu);
+        inflater.inflate(R.menu.forecast_fragment, menu);
     }
 
     @Override
